c3b90c99197b05ad1f557277e3f114bd2aca3491
SumanSingha05 <sumansingha8654@gmail.com>
2024-08-12 00:05:46 +0530

    Initialization done

---
+++ app.js
@@ -0,0 +1,19 @@
+const exp = require("constants");
+const express = require ("express");
+const app = express();
+const http = require("http");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")))
+
+
+app.get("/", function ( req, res)  {
+    res.send("Hello")
+})
+
+server.listen(3000);
